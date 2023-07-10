c2a50212d87f69d9dc9ef821ecf095fd6a074e94
kenser <koffy140221@gmail.com>
2023-07-09 21:03:25 -0600

    crear elementos de html

---
+++ tarea2.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <title>TAREA NO. 2 Manipulación del DOM</title>
+</head>
+<body>
+    <h1 id="tit" class="text-danger">SUBTTE. INF KENSER OMAR CAAL JUC</h1>
+    <h1 id="tit" class="text-danger fw-bold">TAREA NO. 2 "MANIPULACIÓN DEL DOM"</h1>
+    <h1 id="tit" class="text-danger fw-bold">"INGENIERIA DE SOFTWARE"</h1>
+    <div id="parrafo">
+        <p id="parrafo1" class="text-success">La Ingeniería de Software es una de las ramas de las ciencias de la computación que estudia la creación de software confiable y de calidad, basándose en métodos y técnicas de ingeniería, y brindando soporte operacional y de mantenimiento.</p>
+        <p id="parrafo2">Ingeniería de software es el estudio de los principios y metodologías para el desarrollo y mantenimiento de sistemas software (Zelkovitz, 1978).</p>
+        <p id="parrafo3">ngeniería de software es la aplicación práctica del conocimiento científico al diseño y construcción de programas de computadora y a la documentación asociada requerida para desarrollar, operar.</p>
+        <p id="parrafo4">La ingeniería de software trata del establecimiento de los principios y métodos de la ingeniería a fin de obtener software de modo rentable, que sea fiable y trabaje en máquinas reales (Bauer, 1972).</p>
+        <p id="parrafo5">La ingeniería de software es la aplicación de un enfoque sistemático, disciplinado y cuantificable al desarrollo, operación, y mantenimiento del software. </p>
+    </div>
+    <br>
+    <div class="row">
+    <div class="col-md-4">
+              <h3>FORMULARIO DE INFORMACION:</h3>
+              <form>
+              <div>
+                  <label for="text" style="text-align: center;">NOMBRE COMPLETO:</label>
+                  <input class="controles" type="text" class="form-control" id="text" name="text" placeholder="kenseromar@gmail.com" required>
+                </div>
+                <div>
+                  <label for="email" style="text-align: center;">Correo electrónico:</label>
+                  <input class="controles" type="email" class="form-control" id="email" name="email" placeholder="kenseromar@gmail.com" required>
+                </div>
+                <div>
+                  <label for="password" style="text-align: center;">Contraseña:</label>
+                  <input class="controles" type="password" class="form-control" id="password" name="password" placeholder="ingrese su contraseña" required>
+                </div>
+                <a href="../PAGINA PRINCIPAL/PAGINA PRINCIPAL.html"><input class="boton" type="button" name="" value="INGRESAR"> </a><br>
+              </form>
+            </div>
+    </div>
+    <li><i class="fa fa-facebook"></i> <a href="https://www.facebook.com/" target="_blank" ><i class="bi bi-facebook fs-1 text-light"></i> </a>Facebook</li>
+    <li><i class="fa fa-twitter"></i> <a href="https://twitter.com/?lang=es" target="_blank" ><i class="bi bi-twitter fs-1 text-light"></i>  </a>Twitter</li>
+    <li><i class="fa fa-instagram"></i> <a href="https://www.instagram.com/" target="_blank" ><i class="bi bi-instagram fs-1 text-light"></i>  </a>Instagram</li>
+    <script src="scripts.js"></script>
+</body>
+</html>
