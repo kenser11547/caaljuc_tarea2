96e5dc1c9439d33b4f2df7de16f57f9186058f41
kenser <koffy140221@gmail.com>
2023-07-09 21:31:19 -0600

    por medio de selectores

--- script.js
+++ script.js
@@ -2,3 +2,22 @@
 /*SELECCION DE NODOS POR MEDIO DEL ID*/
 let nombretitulo = document.getElementById('tit');
 let tarea = document.getElementById('tit2')
+console.log(nombretitulo)
+console.log(tarea)
+
+/*con los selectores*/
+let enlacepagina = document.querySelector('a');
+let formulariodatos = document.querySelector('form')
+let parrafor = document.querySelectorAll('.text-danger')
+let botoningreso = document.querySelector('#caal')
+let instagram = document.querySelector('a[href*="instagram"]')
+let twitter = document.querySelectorAll('a[href^="https"]')
+console.log(enlacepagina)
+console.log(formulariodatos)
+console.log(parrafor)
+console.log(botoningreso)
+console.log(instagram)
+console.log(twitter)
+
+
+
